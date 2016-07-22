804220c1608e7b5d4e8974c0e4b39b61692264c6
HondaDai <hondadai.tw@gmail.com>
2016-07-23 00:31:55 +0800

    dcard_history

---
+++ dcard_history.js
@@ -0,0 +1,53 @@
+
+
+var fs = require('fs');
+
+var writeFile = (file_path, content) => {
+  fs.writeFile(file_path, content, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    // console.log(`The file '${file_path}' was saved!`);
+  });
+}
+
+
+var header = `
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<style>
+.avatar {
+  height: 300px;
+}
+</style>
+
+`;
+
+var avatars = fs.readdirSync("dcard").map( (x) => {
+
+  return `
+    <div class="col-sm-6 col-md-2">
+      <div class="thumbnail">
+        <img src="dcard/${x}/avatar.jpg" />
+        <div class="caption">
+          <h3>${x}</h3>
+        </div>
+      </div>
+    </div>
+  `
+
+}).join("");
+
+
+var content = `
+${header}
+
+<div class="row">
+${avatars}
+</div>
+`;
+
+writeFile("dcard_history.html", content);
+
+
